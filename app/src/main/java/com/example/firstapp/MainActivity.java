6634ee05ffac4636e02843dbeda599f6e12b0c56
Lord-Tenshi <darkmetroid97@gmail.com>
2020-04-05 21:38:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.firstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        makeText(MainActivity.this, "YEET", LENGTH_SHORT).show();
+    }
+
+    /**
+     * Called when the user taps the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
